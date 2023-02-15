db2847168f923272ce0b1a3b8ccac6d4ef31ad59
Dmitry Litvinenko <ds-sev@ya.ru>
2023-02-15 14:43:57 +0300

    split App to modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import {Product} from './components/Product'
-import {products} from "./data/products";
+import {useProducts} from "./hooks/products";
+import {Product} from "./components/Product";
+import {Loader} from "./components/Loader";
+import {ErrorMessage} from "./components/ErrorMessage";
 
 function App() {
-  return (
-      <div className='container mx-auto max-w-2xl pt-5'>
-        <Product product={products[0]} />
-        <Product product={products[1]} />
+    const {loading, error, products} = useProducts()
 
-      </div>
-  )
+    return (
+        <div className='container mx-auto max-w-2xl pt-5'>
+            {error && <ErrorMessage error={error} />}
+            {loading && <Loader />}
+            {products.map(product => <Product product={product} key={product.id}/>)}
+        </div>
+    )
 }
 
 export default App;
