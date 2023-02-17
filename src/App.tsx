8ef06a548cb5002cd76bd12d2d3a0e325eb2f3b2
Dmitry Litvinenko <ds-sev@ya.ru>
2023-02-17 16:36:15 +0300

    feature: add modal window

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,22 @@ import {useProducts} from "./hooks/products";
 import {Product} from "./components/Product";
 import {Loader} from "./components/Loader";
 import {ErrorMessage} from "./components/ErrorMessage";
+import {Modal} from "./components/Modal";
+import {CreateProduct} from "./components/CreateProduct";
 
 function App() {
     const {loading, error, products} = useProducts()
 
     return (
         <div className='container mx-auto max-w-2xl pt-5'>
-            {error && <ErrorMessage error={error} />}
-            {loading && <Loader />}
+            {error && <ErrorMessage error={error}/>}
+            {loading && <Loader/>}
             {products.map(product => <Product product={product} key={product.id}/>)}
+
+            <Modal title="Create new product">
+                <CreateProduct />
+
+            </Modal>
         </div>
     )
 }
